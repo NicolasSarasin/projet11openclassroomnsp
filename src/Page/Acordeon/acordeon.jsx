9d7c46854e546c184c6c540b1a27bee14bfe6e34
NicolasSarasin <108725344+NicolasSarasin@users.noreply.github.com>
2024-03-20 14:25:49 +0100

    Add files via upload

--- src/Page/Acordeon/acordeon.jsx
+++ src/Page/Acordeon/acordeon.jsx
@@ -1,92 +1,89 @@
-import React, { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import data from "../../Assets/test.json";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import "../../style.css";
+function display(eltid, iconId) {
+    const displayDiv = document.getElementById(eltid);
+    const Icon = document.getElementById(iconId);
+    //const iconAlternate="";
+    console.log("Button Clicked");
+    //switch case with display css in jsx
+    //displayDiv.style.display="none";
+    switch (displayDiv.style.display) {
+        case "block":
+            displayDiv.style.display = "";
+            displayDiv.style.transform = "translateY(0px)";
+            displayDiv.style.animation = "none 1s";
+            Icon.style.transform = "rotate(0deg)";
+            Icon.style.marginTop = "17.5px";
+            break;
+        default:
+            displayDiv.style.display = "block";
+            displayDiv.style.transform = "translateY(2.5px)";
+            displayDiv.style.animation = "block 1s";
+            Icon.style.transform = "rotate(180deg)";
+            Icon.style.marginBottom = "17.5px";
+    }
+}
 
-function Acordeon() {
-    const { id } = useParams();
+function Acordeon(props1, props2) {
+    const acordeon1 = props1.description;
+    const acordeon2 = props2.equipments;
     const navigate = useNavigate();
-    const [apartment, setApartment] = useState(null);
-    const [pictureIndex, setPictureIndex] = useState(0);
-    useEffect(() => {
-        const foundApartment = data.find((item) => item.id === id);
-        if (foundApartment === undefined) {
-            navigate("/Error");
-        } else {
-            setApartment(foundApartment);
-        }
-    }, [id, navigate]);
-    if (!apartment) {
-        // If apartment is not found, the useEffect will handle the redirection, so you can just return null here
-        return null;
+    if (acordeon1 === undefined) {
+        navigate("/Error");
     }
-    const imageNext = () => {
-        setPictureIndex((pictureIndex + 1) % apartment.pictures.length);
-    };
-    const imagePrevious = () => {
-        setPictureIndex(
-            (pictureIndex + apartment.pictures.length - 1) %
-                apartment.pictures.length
-        );
-    };
-
+    if (acordeon2 === undefined) {
+        navigate("/Error");
+    }
     // If apartment is not found, the useEffect will handle the redirection, so you can just return null here
-    if (!apartment) {
-        return null;
-    }
-    /*return (
+
+    return (
         <div className="divCardDescriptionGeneral">
-                <div className="divCardDescription">
-                    <div className="divCardDescription1">
-                        <h2>Description</h2>
-                        <i
-                            className="iconDescription"
-                            id="LocationIcon"
-                            onClick={() =>
-                                display("locationDetails", "LocationIcon")
-                            }
-                        >
-                            <img
-                                src="./image/arrow_back_ios-24px 2.png"
-                                alt="arrow"
-                            />
-                        </i>
-                    </div>
-                    <div
-                        id="locationDetails"
-                        className="divCardDescriptionUnder"
+            <div className="divCardDescription">
+                <div className="divCardDescription1">
+                    <h2>Description</h2>
+                    <i
+                        className="iconDescription"
+                        id="LocationIcon"
+                        onClick={() =>
+                            display("locationDetails", "LocationIcon")
+                        }
                     >
-                        <p>{apartment.description}</p>
-                    </div>
+                        <img
+                            src="./image/arrow_back_ios-24px 2.png"
+                            alt="arrow"
+                        />
+                    </i>
                 </div>
-                <div className="divCardDescription">
-                    <div className="divCardDescription1">
-                        <h2>Équipements</h2>
-                        <i
-                            className="iconDescription"
-                            id="EquipmentIcon"
-                            onClick={() =>
-                                display("EquipmentDetails", "EquipmentIcon")
-                            }
-                        >
-                            <img
-                                src="./image/arrow_back_ios-24px 2.png"
-                                alt="arrow"
-                            />
-                        </i>
-                    </div>
-                    <div
-                        id="EquipmentDetails"
-                        className="divCardDescriptionUnder"
-                    >
-                        {apartment.equipments.map((equipment, index) => (
-                            <p key={"equipment" + index}>{equipment}</p>
-                        ))}
-                    </div>
+                <div id="locationDetails" className="divCardDescriptionUnder">
+                    <p>{acordeon1}</p>
                 </div>
             </div>
-    );*/
+            <div className="divCardDescription">
+                <div className="divCardDescription1">
+                    <h2>Équipements</h2>
+                    <i
+                        className="iconDescription"
+                        id="EquipmentIcon"
+                        onClick={() =>
+                            display("EquipmentDetails", "EquipmentIcon")
+                        }
+                    >
+                        <img
+                            src="./image/arrow_back_ios-24px 2.png"
+                            alt="arrow"
+                        />
+                    </i>
+                </div>
+                <div id="EquipmentDetails" className="divCardDescriptionUnder">
+                    {acordeon2.map((equipment, index) => (
+                        <p key={"equipment" + index}>{equipment}</p>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Acordeon;
