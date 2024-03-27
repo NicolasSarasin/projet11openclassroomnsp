62b48ab179627b66185cb848308e3902fc84ed42
NicolasSarasin <108725344+NicolasSarasin@users.noreply.github.com>
2024-03-27 10:08:55 +0100

    Add files via upload

--- src/index.jsx
+++ src/index.jsx
@@ -2,33 +2,8 @@ import "./style.css";
 //import { Link } from "react-router-dom";
 import Header from "./Page/Header_footer/Header.jsx";
 import Footer from "./Page/Header_footer/Footer.jsx";
-//import React, { useState } from "react";
-/*import ReactDOM from "react-dom/client";
-<FontAwesomeIcon icon={["fab", "github"]}/>*/
-//import "@fortawesome/fontawesome-free/css/all.min.css";
-function display(eltid, iconId) {
-    const displayDiv = document.getElementById(eltid);
-    const Icon = document.getElementById(iconId);
-    //const iconAlternate="";
-    console.log("Button Clicked");
-    //switch case with display css in jsx
-    //displayDiv.style.display="none";
-    switch (displayDiv.style.display) {
-        case "block":
-            displayDiv.style.display = "";
-            displayDiv.style.transform = "translateY(0px)";
-            displayDiv.style.animation = "none 1s";
-            Icon.style.transform = "rotate(0deg)";
-            Icon.style.marginTop = "17.5px";
-            break;
-        default:
-            displayDiv.style.display = "block";
-            displayDiv.style.transform = "translateY(2.5px)";
-            displayDiv.style.animation = "block 1s";
-            Icon.style.transform = "rotate(180deg)";
-            Icon.style.marginBottom = "17.5px";
-    }
-}
+import Acordeon from "./Page/Acordeon/acordeon.jsx";
+
 function Index() {
     return (
         <div className="App">
@@ -38,6 +13,33 @@ function Index() {
                 alt="ByTheWay"
                 className="ImageMain"
             />
+            <Acordeon
+              id="fiability"
+              title="Fiabilité"
+              details="Les annonces posté sur Kasa garantisent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." 
+            />
+            <Acordeon
+              id="respect"
+              title="Respect"
+              details="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation de voisinage entraînera une exclusion de notre plateforme."
+            />
+            <Acordeon
+              id="service"
+              title="Service"
+              details="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation de voisinage entraînera une exclusion de notre plateforme."
+            />
+            <Acordeon
+              id="security"
+              title="Sécurité"
+              details=" La sécurité est la priorité de Kasa. Aussi bien que pour nos
+                    hôtes que pour les voyageurs, chaque logements correspond
+                    aux critères de sécurités établis par nos services. En
+                    laissant une nôte aussi bien qu'à l'hôte qu'au locataire,
+                    cela permet à nos équipes de vérifier les standards sont
+                    bien respectés. Nos organisons également des ateliers sur la
+                    sécurité domestique pour nos hôtes."
+            />
+            {/*
             <div className="DivByTheWay1">
                 <h2>Fiabilité</h2>
                 <i
@@ -113,6 +115,7 @@ function Index() {
                     sécurité domestique pour nos hôtes.
                 </p>
             </div>
+            */}
             <Footer />
         </div>
     );
